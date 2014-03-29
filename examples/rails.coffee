4bfcf2a0c4149d0e19051f95bbe1bd8bf5226d96
Kristoffer Walker <kris@kixx.name>
2014-03-29 00:24:07 -0400

    WIP, but file glob reading is working.
    	new file:   examples/rails.coffee
    	modified:   index.js
    	new file:   lib/configurator.js
    	modified:   lib/index.js
    	new file:   lib/read_config_performer.js
    	modified:   package.json

---
+++ examples/rails.coffee
@@ -0,0 +1,4 @@
+exports.config = (config) ->
+  config.files "app/assets/**"
+  config.files "vendor/assets/**"
+  return
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-Promise = require('iou').Promise
+Promise = require('iou').Promise;
+require('coffee-script').register();
 
 var HTTP = require('http')
   , URL = require('url')
@@ -13,6 +14,7 @@ var HTTP = require('http')
 exports.main = function () {
   var opts = parse_options()
     , argv = opts.argv
+    , run_opts = Object.create(null)
 
   if (argv.help) {
     console.log(opts.help())
@@ -25,61 +27,52 @@ exports.main = function () {
     process.exit(2);
   }
 
-  exports.run({connection_uri: argv.serve, target_uri: argv.target}, function (address) {
-    var hostname = address.address
-      , port = address.port
-      , family = address.family
+  run_opts.connection_uri = argv.serve;
+  run_opts.target_uri = argv.target;
+  run_opts.config_path = argv.config;
 
-    console.log('Listening for connections on '+ hostname +':'+ port +' '+ family);
-    console.log('Proxying requests to '+ argv.target);
-    console.log('Press CTRL+c to stop.')
-  });
+  exports.run(run_opts).then(print_output).catch(die);
 };
 
 
+// opts.connection_uri - The URI String of the dev_proxy server.
+// opts.target_uri - The URI String of the target server.
+// opts.config_path - The path String of the configuration file.
+// callback - Callback Function is optional.
 exports.run = function (opts, callback) {
-  var server
-    , proxy = create_proxy(opts.target_uri)
-    , connection_uri = URL.parse(opts.connection_uri)
+  var state = Object.create(null)
 
-  server = HTTP.createServer(function (req, res) {
-    var cached_response
-    if (cached_response = LIB.cache_service.get(req)) {
-      cached_response.pipe(res);
-    } else {
-      proxy(req, res);
+  if (opts.config_path) {
+    state.config_path =  PATH.newPath(opts.config_path);
+  } else {
+    state.config_path = PATH.newPath(__dirname).append('examples', 'rails');
+  }
+
+  state.connection_uri = opts.connection_uri;
+  state.target_uri = opts.target_uri;
+
+  function on_success(state) {
+    if (typeof callback === 'function') {
+      callback(null, state);
     }
-  });
+    return state;
+  }
 
-  server.listen(connection_uri.port, connection_uri.hostname, function () {
-    return callback(server.address());
-  });
-};
+  function on_error(err) {
+    if (typeof callback === 'function') {
+      callback(err);
+    } else {
+      return Promise.reject(err);
+    }
+  }
 
-exports.run = function (opts, callback) {
-  var promise = Promise.resolve({})
-
-  promise = Promise.resolve(Object.create(null))
-    .then(LIB.middleware.config_reader({path: }))
-    .then(LIB.middleware.create_watcher())
-    .then(LIB.middleware.create_router())
-    .then(LIB.middleware.create_server({
-      connection_uri: opts.connection_uri
-    , target_uri: opts.target_uri
-    }))
-    .then(function (state) {
-      if (typeof callback === 'function') {
-        callback(null, state);
-      }
-      return state;
-    })
-    .fail(function (err) {
-      if (typeof callback === 'function') {
-        callback(err);
-      } else {
-        return Promise.reject(err);
-      }
-    });
+  var promise = Promise.resolve(state)
+    .then(LIB.middleware.config_reader)
+    .then(LIB.middleware.file_watcher)
+    .then(LIB.middleware.path_matcher)
+    .then(LIB.middleware.http_server)
+    .then(on_success)
+    .catch(on_error);
 
   return promise;
 };
@@ -92,14 +85,31 @@ function parse_options() {
     .alias('s', 'serve')
     .describe('s', 'Server URI like "http://192.168.1.102:8080"')
     .default('s', 'http://localhost:8080')
+    .alias('c', 'config')
+    .describe('c', 'Configuraiton file path')
     .alias('h', 'help')
     .describe('h', "Print this help text.")
 }
 
 
+function print_output(state) {
+  console.log('STATE', state);
+  console.log('Listening for connections on '+ state.hostname +':'+ state.port +' '+ state.family);
+  console.log('Proxying requests to '+ state.target_uri);
+  console.log('Press CTRL+c to stop.');
+}
+
+function die(err) {
+  console.error(err.stack);
+  process.exit(2);
+}
+
+
+/*
 function create_proxy(target) {
   return function (req, res) {
     var buffer = LIB.cache_service.cache(req)
     req.pipe(REQUEST(target + req.url)).pipe(buffer).pipe(res);
   };
 }
+*/
